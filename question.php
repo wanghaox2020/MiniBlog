e536b15e38198dee3d0e69a9c74a59b7506a5de0
wanghaox2020 <79470452+wanghaox2020@users.noreply.github.com>
2022-05-11 20:11:11 -0400

    udpate question search bar

--- question.php
+++ question.php
@@ -1,74 +1,37 @@
 <!doctype html>
 <html lang="en">
-  
-  </head>
+<head>
+    <style>
+        h1 {text-align: center;
+            font-size: 30pt;
+            }
+        p{ text-align: left;
+            font-size: 15pt;
+        }
+        div{
+            width: 15pt;
+        }
+
+    </style>  
+</head>
 
 
-<!--Section: FAQ-->
+<!--Section: search part-->
 <section>
-  <h3 class="text-center mb-8 pb-2 text-primary fw-bold">Question and Answer Page</h3>
-  <p class="text-center mb-5">
-    Find the answers for the most recent asked questions below
-  </p>
+  <h1 class="text-center mb-8 pb-2 text-primary fw-bold">Question and Answer Page</h3>
+    <p class="text-center mb-5">Find a question in our database ~ </p>
 
-  <div class="row">
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i> A simple
-        question?</h6>
-      <p>
-        <strong><u>Absolutely!</u></strong> We work with top payment companies which guarantees
-        your
-        safety and
-        security. All billing information is stored on our payment processing partner.
-      </p>
+    <div class="container">
+        <div class="row height d-flex justify-content-center align-items-center">
+            <div class="col-md-8">
+                <div class="search">
+                    <i class="fa fa-search"></i>
+                        <input type="text" class="form-control" placeholder="Have a question? Ask Now">
+                        <button class="btn btn-primary">Search</button>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i> A question
-        that
-        is longer then the previous one?</h6>
-      <p>
-        <strong><u>Yes, it is possible!</u></strong> You can cancel your subscription anytime in
-        your
-        account. Once the subscription is
-        cancelled, you will not be charged next month.
-      </p>
-    </div>
-
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i> A simple
-        question?
-      </h6>
-      <p>
-        Currently, we only offer monthly subscription. You can upgrade or cancel your monthly
-        account at any time with no further obligation.
-      </p>
-    </div>
-
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i> A simple
-        question?
-      </h6>
-      <p>
-        Yes. Go to the billing section of your dashboard and update your payment information.
-      </p>
-    </div>
-
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i> A simple
-        question?
-      </h6>
-      <p><strong><u>Unfortunately no</u>.</strong> We do not issue full or partial refunds for any
-        reason.</p>
-    </div>
-
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> Another
-        question that is longer than usual</h6>
-      <p>
-        Of course! We’re happy to offer a free plan to anyone who wants to try our service.
-      </p>
-    </div>
-  </div>
 </section>
-<!--Section: FAQ-->
+<!--Section: search part-->
