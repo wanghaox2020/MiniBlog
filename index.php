69d863301cc830c12ebbb7853881a7fa1a4c1a62
Tongxuan Wang <tw2484@nyu.edu>
2022-05-12 19:19:31 -0400

    home page and post page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author">
     <meta name="generator" content="Hugo 0.88.1">
     <title>Signin</title>
 
@@ -38,7 +38,7 @@
 
     
   </head>
-  <body class="text-center">
+<body class="text-center">
     
 <main class="form-signin">
   <form method="POST">
