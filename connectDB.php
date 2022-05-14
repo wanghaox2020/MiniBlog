1292d8f4c6f54d4e01f443dba3bd6c1ee1edf1a8
Tongxuan Wang <tw2484@nyu.edu>
2022-05-14 06:45:22 -0400

    thumbs up

--- connectDB.php
+++ connectDB.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $dbUsername = "root";
-$dbPassword = "Whx19961007.";
+$dbPassword = "wtx20150914";
 $dbName = "Project";
 
 $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
